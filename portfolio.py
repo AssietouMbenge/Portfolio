cb93ff4575ae27f8617be73961bc4052d1fdbbf3
AssietouMbenge <assylay20@icloud.com>
2026-02-17 18:58:07 +0000

    Add files via upload

---
+++ portfolio.py
@@ -0,0 +1,53 @@
+import streamlit as st 
+with st.sidebar:
+    st.title("Assietou Laye Mbengue")
+    st.subheader("Geomaticienne")
+    st.image("assia.jpg",width=170)
+    st.write("Email : assialayemb@gmail.com")
+    st.write("Telephone : +221 77 869 72 77")
+    st.write("Adresse : Dakar, Senegal")
+
+st.header("A propos de moi")
+st.write("Technicienne superieur en geomatique, passionée par l'analyse spatiale et l'amenagement du territoire. Interessée par la conception de batiment. J'aime notamment la programmation et le travail sur les bases de données spatiales")
+#================================================================================
+st.header("Competence")
+st.markdown("""
+
+* Systèmes d'Information Géographique (SIG) : Maîtrise de logiciels comme QGIS, ArcGIS.
+* Gestion de bases de données : Conception, structuration, requêtage SQL, manipulation de données géographiques (raster/vecteur).
+* Cartographie et visualisation : Production de cartes thématiques, représentations graphiques.
+* Informatique et programmation : Langages de script (Python), langages web, automatisation de traitements , deploiement.
+* Réalisation de plan 2D et 3D sur Autocad et Sketchup
+
+""")
+
+st.header("Experiences")
+st.markdown("""
+* Digitalisation et correction topologique de  quartier sur Qgis et Arcgis.
+
+* Réalisation de plan 2D(RDC, Etage, Terrasse, Masse, Coupe et Façade) sur Autocad et ensuite importation sur Sketchup pour la construction 3D)
+
+* Collecte de données  de la commune de Fass-Colobane-Gueule tapée avec l'application Mobile Topographer puis traitement des données sur Arcgis pour la gestion fiscale.
+
+* Réalisation de carte de localisation et de carte thématique des types de sols d'une région du Sénégal.
+
+* Création d'application web avec le module streamlit sur JupiterLab
+
+* Différentes tâche de codage avec le language python sur IDLE, jupiter notebook et sur jupiterlab
+
+* Création de base de données sur Qgis (géopackage et spatialite) et sur Arcgis (file géodatabase et personal géodatabase)
+""")
+
+
+
+st.header("Activités")
+st.markdown("""
+- Sport
+- Photographie
+""")
+
+
+
+
+    
+    
