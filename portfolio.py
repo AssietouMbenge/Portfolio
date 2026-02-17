7a707bfd91ffb9649c8577c168ee5fe8f1c97a0d
AssietouMbenge <assylay20@icloud.com>
2026-02-17 19:25:59 +0000

    Update portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -1,11 +1,11 @@
 import streamlit as st 
 ###with st.sidebar:
-    st.title("Assietou Laye Mbengue")
-    st.subheader("Geomaticienne")
-    st.image("assia.jpg",width=170)
-    st.write("Email : assialayemb@gmail.com")
-    st.write("Telephone : +221 77 869 72 77")
-    st.write("Adresse : Dakar, Senegal")
+ st.title("Assietou Laye Mbengue")
+ st.subheader("Geomaticienne")
+ st.image("assia.jpg",width=170)
+ st.write("Email : assialayemb@gmail.com")
+ st.write("Telephone : +221 77 869 72 77")
+ st.write("Adresse : Dakar, Senegal")
 
 st.header("A propos de moi")
 st.write("Technicienne superieur en geomatique, passionée par l'analyse spatiale et l'amenagement du territoire. Interessée par la conception de batiment. J'aime notamment la programmation et le travail sur les bases de données spatiales")
